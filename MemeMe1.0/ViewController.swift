764da4c6f05e28d5a6312afd87749ecf8fa17b60
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-09-15 15:29:41 +0200

    Initial Commit

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by David Koch on 15.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
