d42b5fe0bab78fe0610e0478d75e217c95cd0d67
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-09-21 15:44:52 +0200

    Auto Layout problems when trying different orientations or devices. Only font color is defined and not stroke due to not centering or not applying color if added.

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -7,13 +7,171 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var navbar: UINavigationBar!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+
+        configureText(textField: bottomTextField, text: "BOTTOM")
+        configureText(textField: topTextField, text: "TOP")
+
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        if imagePickerView.image != nil {
+            shareButton.isEnabled = true
+        }
+        else {
+            shareButton.isEnabled = false
+        }
+        
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Subscribe to the keyboard notifications, to allow the view to raise when necessary
+        subscribeToKeyboardNotifications()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+            shareButton.isEnabled = true
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func keyboardAppearance(_ notification: Notification) {
+        
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    @objc func keyboardDisappearance(_ notification: Notification) {
+        view.frame.origin.y = .zero
+    }
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppearance(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappearance(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        let memedImage = generateMemedImage()
+        let activityVC = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        present(activityVC, animated: true) {
+            self.save()
+        }
+
+        
+        
+    }
+    @IBAction func cancel(_ sender: Any) {
+        configureText(textField: bottomTextField, text: "BOTTOM")
+        configureText(textField: topTextField, text: "TOP")
+        imagePickerView.image = nil
+        shareButton.isEnabled = false
+    }
+    
+    func save() {
+        
+        // Create the meme
+        let memedImage = generateMemedImage()
+        let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imagePickerView.image!, memedImage: memedImage)
+        
+    }
+    
+    func generateMemedImage() -> UIImage {
+        //Hide navigation and tool bar
+        
+        toolbar.isHidden = true
+        navbar.isHidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        toolbar.isHidden = false
+        navbar.isHidden = false
+        return memedImage
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
+    }
+}
+
+func configureText(textField: UITextField, text: String){
+    
+    textField.autocapitalizationType = .allCharacters
+    textField.text = text
+    textField.defaultTextAttributes = memeTextAttributes
+    textField.defaultTextAttributes = memeTextAttributes
+    textField.textAlignment = .center
+    
+}
 
 
+
+let memeTextAttributes: [NSAttributedString.Key: Any] = [
+    
+    NSAttributedString.Key.foregroundColor: UIColor.white,
+    //NSAttributedString.Key.strokeColor: UIColor.black,
+    NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    //NSAttributedString.Key.strokeWidth: 3,
+
+    
+]
+
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
 }
 
